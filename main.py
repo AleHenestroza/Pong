daaa5bde789d008e436ec6ce145874d5f9dc1a63
Alejandro Henestroza <alehenestroza@gmail.com>
2021-02-28 16:23:59 -0300

    Ajustada detección de colisión

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ while running:
         ball.bounce_y()
 
     # Detección de colisión con las paletas
-    if (ball.distance(r_paddle) < 50 and ball.xcor() > 330) or (ball.distance(l_paddle) < 50 and ball.xcor() < -330):
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
         ball.bounce_x()
 
 
--- paddle.py
+++ paddle.py
@@ -10,7 +10,7 @@ class Paddle(Turtle):
         super().__init__("square")
         self.penup()
         self.color("white")
-        self.shapesize(stretch_wid=PADDLE_WIDTH, stretch_len=1)
+        self.shapesize(stretch_wid=PADDLE_WIDTH, stretch_len=1)  # Inicial: 20x20 - Stretched: 100x20
         self.goto(x=coordinates[0], y=coordinates[1])
 
     def up(self):
